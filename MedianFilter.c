431fe5c1c0c7bf3af54f0aa053173eeb3c11b45a
shuvangkar <sshuvo93@gmail.com>
2021-03-06 16:30:02 +0600

    added decimation filter

--- MedianFilter.c
+++ MedianFilter.c
@@ -1,8 +1,12 @@
 #include "MedianFilter.h"
+#include "Serial.h"
 
 uint16_t *_buf;
 uint8_t _N;
 uint8_t _index;
+
+uint16_t average(uint16_t *samples, uint8_t N);
+
 void medFilterSet(uint16_t *buf, uint8_t N)
 {
   _buf = buf;
@@ -35,11 +39,55 @@ int cmpfunc (const void * a, const void * b)
    return ( *(int*)a - *(int*)b );
 }
 
-uint16_t medFilterOut()
+
+void medPrintSamples(uint16_t *data, uint8_t samples)
 {
+	for (uint8_t i = 0; i< samples; i++)
+	{
+		SerialPrintU16(data[i]); SerialPrintF(P("  "));
+	}
+	SerialPrintF(P("\r\n"));
+	
+}
+
+uint16_t average(uint16_t *samples, uint8_t N)
+{
+	uint16_t sum = 0;
+	for(uint8_t i = 0; i<N; i++)
+	{
+		sum += samples[i];
+	}
+	sum = sum/N;
+	return sum;
+}
+
+uint16_t medFilterOut(filter_type_t type)
+{
+
+  //medPrintSamples(_buf,_N);
   uint16_t tempBuf[_N];
   memcpy(tempBuf,_buf,_N*sizeof(uint16_t));
+  
+  uint16_t out;
+  switch(type)
+  {
+	  case MEDIAN:
+		qsort(tempBuf,_N,sizeof(uint16_t),cmpfunc);
+		out = tempBuf[(_N-1)/2];
+	  break;
+	  case DECIMATION:
+		out = average(tempBuf,_N);
+	  break;
+  } 
+  SerialPrintF(P("Filter out : ")); SerialPrintlnU16(out);
+  return out;
+  
+  /*
   qsort(tempBuf,_N,sizeof(uint16_t),cmpfunc);
+  //medPrintSamples(tempBuf,_N);
   uint16_t medIndex = (_N-1)/2;
+  uint16_t avg = average(tempBuf,_N);
+  SerialPrintF(P("Avg : ")); SerialPrintlnU16(avg);
   return  tempBuf[medIndex];
+  */
 }
--- MedianFilter.h
+++ MedianFilter.h
@@ -2,12 +2,23 @@
 #define _MEDIAN_FILTER_H
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdbool.h>
+
+typedef enum filter_type_t
+{
+	MEDIAN,
+	DECIMATION
+}filter_type_t;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 void medFilterSet(uint16_t *buf, uint8_t N);
 void medFilterPush(uint16_t point);
-uint16_t medFilterOut();
+uint16_t medFilterOut(filter_type_t type);
+void medPrintSamples(uint16_t *data, uint8_t samples);
 
 extern uint16_t *_buf;
 #ifdef __cplusplus
